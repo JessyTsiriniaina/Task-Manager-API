f22af5d5ae89fb9c7822af680fe42fe459012d4e
Tsiriniaina Jessy <tsiriniaina.jessy@gmail.com>
2026-08-14 19:50:14 +0300

    initialize project

---
+++ src/main/java/io/jessytsiriniaina/taskmanagerapi/TaskManagerApiApplication.java
@@ -0,0 +1,13 @@
+package io.jessytsiriniaina.taskmanagerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskManagerApiApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(TaskManagerApiApplication.class, args);
+    }
+
+}
