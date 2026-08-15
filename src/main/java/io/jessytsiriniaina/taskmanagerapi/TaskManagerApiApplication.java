84d658c90a03f4a294f958d921da6c2998795bff
Tsiriniaina Jessy <tsiriniaina.jessy@gmail.com>
2026-08-15 17:01:42 +0300

    feat: add OPENAPI/Swager documentation feat: configure postgresql database

--- src/main/java/io/jessytsiriniaina/taskmanagerapi/TaskManagerApiApplication.java
+++ src/main/java/io/jessytsiriniaina/taskmanagerapi/TaskManagerApiApplication.java
@@ -1,7 +1,17 @@
 package io.jessytsiriniaina.taskmanagerapi;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Task Manager API",
+                version = "1.0",
+                description = "API documentation for task manager"
+        )
+)
 
 @SpringBootApplication
 public class TaskManagerApiApplication {
