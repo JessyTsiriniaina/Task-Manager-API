d44ffe6de99d91d69b5acbe30b42e1ae5da36425
Tsiriniaina Jessy <tsiriniaina.jessy@gmail.com>
2026-08-18 12:15:30 +0300

    fix(TaskController): remove dead code line in import section

--- src/main/java/io/jessytsiriniaina/taskmanagerapi/controller/TaskController.java
+++ src/main/java/io/jessytsiriniaina/taskmanagerapi/controller/TaskController.java
@@ -7,8 +7,6 @@ import io.jessytsiriniaina.taskmanagerapi.service.TaskService;
 import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
